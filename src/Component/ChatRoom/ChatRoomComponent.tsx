ad1ecc924672601850bb02ad46eb5fdda4429a4e
AntonioContrerasRomán <antoniocontrerasroman@gmail.com>
2019-02-24 19:18:53 +0100

    More renames

--- src/Component/ChatRoom/ChatRoomComponent.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import { RoomTitleComponent } from './Components/roomTitle';
-
-export const ChatRoomComponent: React.StatelessComponent = () => (
-  <>
-    <RoomTitleComponent />
-  </>
-);
---
+++ src/components/chatRoom/chatRoom.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import { RoomTitleComponent } from './components/roomTitleComponent';
+
+export const ChatRoom: React.StatelessComponent = () => (
+  <>
+    <RoomTitleComponent />
+  </>
+);

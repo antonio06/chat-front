55e3be088d2f5de285890c864f46f569a9406ddd
AntonioContrerasRomán <antoniocontrerasroman@gmail.com>
2019-03-02 22:33:50 +0100

    Fix word

--- src/components/chatRoom/components/card/card.tsx
+++ src/components/chatRoom/components/card/card.tsx
@@ -5,7 +5,7 @@ export const Card: React.StatelessComponent = () => (
   <div css={styles.wrapper}>
     <h3 css={styles.user}>Adrian Von Ziegler</h3>
     <div css={styles.card}>
-      <p css={styles.message}>Hello everyone, What are you doing ?. I'm work right now</p>
+      <p css={styles.message}>Hello everyone, What are you doing ?. I'm working right now</p>
     </div>
   </div>
 );
